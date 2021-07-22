f597f327a2a8e7e7a1fb22353ac5bf68fc7380f1
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2021-07-22 13:46:24 +0800

    Custom env

--- src/common/config/erp.config.ts
+++ src/common/config/erp.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('erp', () => ({
-  baseUrl: process.env.ERP_API_BASE_URL,
-  apiKey: process.env.ERP_API_KEY,
+  baseUrl: process.env.APPSETTING_ERP_API_BASE_URL,
+  apiKey: process.env.APPSETTING_ERP_API_KEY,
 }));
