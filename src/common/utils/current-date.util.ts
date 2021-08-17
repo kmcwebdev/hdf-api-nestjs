933e95ada0668f95e07c557089a56fa1f2ef9bb2
csulit <chrisgelosulit@gmail.com>
2021-08-17 09:53:00 +0800

    Make new date to locale date string

--- src/common/utils/current-date.util.ts
+++ src/common/utils/current-date.util.ts
@@ -1,7 +1,10 @@
 import { format } from 'date-fns';
 
 export function currentDate() {
-  const date = `${format(new Date(), 'yyyy-MM-dd')}T00:00:00.000Z`;
+  const date = `${format(
+    new Date(new Date().toLocaleDateString()),
+    'yyyy-MM-dd',
+  )}T00:00:00.000Z`;
 
   return new Date(date);
 }
