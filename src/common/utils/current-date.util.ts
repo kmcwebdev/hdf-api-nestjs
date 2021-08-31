df289ccf40b901a742d46fd76d4bbbf687a8ce1f
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2021-08-31 13:24:30 +0800

    Update prisma package

--- src/common/utils/current-date.util.ts
+++ src/common/utils/current-date.util.ts
@@ -1,7 +1,7 @@
-//import { format } from 'date-fns';
+import { format } from 'date-fns';
 
 export function currentDate() {
-  //const date = `${format(new Date(), 'yyyy-MM-dd')}T00:00:00.000Z`;
+  const date = `${format(new Date(), 'yyyy-MM-dd')}T00:00:00.000Z`;
 
-  return new Date(/* date */);
+  return new Date(date);
 }
